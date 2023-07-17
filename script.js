a82f4acf3860bd6fc798aafbb470b432380a3fbc
rucha-kulkarni <118054484+rucha-kulkarni@users.noreply.github.com>
2023-07-17 22:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let str = prompt() ;
+
+let obj = {}
+
+for(let i = 0; i<str.length; i++){
+	if(obj[str[i]]){
+		obj[str[i]]++;
+	}
+	else{
+		obj[str[i]] = 1;
+	}
+}
+
+for(let i=0; i<str.length;i++){
+	if(obj[str[i]] === 1){
+		alert(str[i]);
+		break;
+	}
+}
